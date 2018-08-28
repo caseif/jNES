d48709a783043c3b9a9967e52da557cc859b15ee
Max Roncace <me@caseif.net>
2018-08-28 13:27:31 -0400

    More addition tests

--- src/test/java/net/caseif/jnes/emulation/cpu/AdditionTest.java
+++ src/test/java/net/caseif/jnes/emulation/cpu/AdditionTest.java
@@ -10,13 +10,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MathTest {
+public class AdditionTest {
 
     private static CpuInterpreter ci;
 
     @BeforeAll
     public static void init() throws IOException {
-        ci = loadPrg("/cpu_tests/math.bin");
+        ci = loadPrg("/cpu_tests/addition.bin");
     }
 
     @Test
@@ -47,6 +47,27 @@ public class MathTest {
         assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.CARRY));
         assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.ZERO));
         assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.OVERFLOW));
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.NEGATIVE));
+
+        CpuTestHelper.runCpuOnce(ci);
+        assertEquals(0x02, ci.regs.getAcc());
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.CARRY));
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.ZERO));
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.OVERFLOW));
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.NEGATIVE));
+
+        CpuTestHelper.runCpuOnce(ci);
+        assertEquals(0x80, ci.regs.getAcc());
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.CARRY));
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.ZERO));
+        assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.OVERFLOW));
+        assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.NEGATIVE));
+
+        CpuTestHelper.runCpuOnce(ci);
+        assertEquals(0x00, ci.regs.getAcc());
+        assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.CARRY));
+        assertTrue(ci.getStatus().getFlag(CpuStatus.Flag.ZERO));
+        assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.OVERFLOW));
         assertFalse(ci.getStatus().getFlag(CpuStatus.Flag.NEGATIVE));
     }
 
--- src/test/resources/cpu_tests/addition.asm
+++ src/test/resources/cpu_tests/addition.asm
@@ -1,16 +1,16 @@
 ;;;;;;;;;;;;;;;;
-; test math operations
+; test addition
 ;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;
-; test addition
+; without carry
 ;;;;;;;;;;;;;;;;
 
 CLC             ; clear carry bit
 LDA #$01        ; init acc
 ADC #$01        ; add 1 to acc
 NOP             ; perform assertions:
-                ; A = $02
+                ; A = 0x02
                 ; C = 0
                 ; Z = 0
                 ; V = 0
@@ -20,7 +20,7 @@ CLC             ; clear carry bit
 LDA #$FE        ; init acc
 ADC #$03        ; add 3 to acc, overflowing it
 NOP             ; perform assertions:
-                ; A = $01
+                ; A = 0x01
                 ; C = 1
                 ; Z = 0
                 ; V = 0
@@ -30,7 +30,7 @@ CLC             ; clear carry bit
 LDA #$40        ; init acc
 ADC #$40        ; add 64 to acc, overflowing the signed result
 NOP             ; perform assertions:
-                ; A = $80
+                ; A = 0x80
                 ; C = 0
                 ; Z = 0
                 ; V = 1
@@ -40,8 +40,42 @@ CLC             ; clear carry bit
 LDA #$80        ; init acc
 ADC #$80        ; add 128/-128, overflowing the accumulator to 0
 NOP             ; perform assertions:
-                ; A = $00
+                ; A = 0x00
                 ; C = 1
                 ; Z = 1
                 ; V = 1
+                ; N = 0
+
+;;;;;;;;;;;;;;;;
+; with carry
+;;;;;;;;;;;;;;;;
+
+SEC             ; set carry bit
+LDA #$00        ; init acc
+ADC #$01        ; add 1 to acc
+NOP             ; perform assertions:
+                ; A = 0x02
+                ; C = 0
+                ; Z = 0
+                ; V = 0
+                ; N = 0
+
+SEC             ; set carry bit
+LDA #$7E        ; init acc
+ADC #$01        ; add 1 to acc, overflowing the signed result
+NOP             ; perform assertions:
+                ; A = 0x80
+                ; C = 0
+                ; Z = 0
+                ; V = 1
+                ; N = 1
+
+SEC             ; set carry bit
+LDA #$FE        ; init acc
+ADC #$01        ; add 1 to acc, overflowing the unsigned result
+NOP             ; perform assertions:
+                ; A = 0x00
+                ; C = 1
+                ; Z = 1
+                ; V = 0
                 ; N = 0
